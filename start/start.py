e0b5a74319c8612094a9a2306f74abd34d2ffd41
Sachin.Rane <Sachin.Rane@ISIPL-PUNV-L064.corp.intelliswift.com>
2019-01-10 19:43:29 +0530

    new commands

--- start/start.py
+++ start/start.py
@@ -1,1 +1,24 @@
-import this
+import this #for python fun
+
+print("Hello World!!")
+
+#variable assignment
+name = 'Sachin'
+print(name)
+
+#mathematical calculation
+
+#print('addition='+a) 
+#print('substraction='+a) 
+#print('multiplication='+a) 
+#print('division='+a) 
+
+#repeat string
+print('Sachin!' *5)
+#output - Sachin!Sachin!Sachin!Sachin!Sachin!
+
+#print current dir
+import os
+print(os.getcwd())
+
+
